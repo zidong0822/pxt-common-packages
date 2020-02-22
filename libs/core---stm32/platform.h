c3a866c3028033b1addba55dad9b1fd2d5672dad
zi.wang <zi.wang@dixv.onmicrosoft.com>
2020-02-22 17:22:49 +0800

    fix time pllr && fix led

--- libs/core---stm32/platform.h
+++ libs/core---stm32/platform.h
@@ -6,6 +6,7 @@
 #include "ZPin.h"
 #include "Timer.h"
 #include "ZSPI.h"
+#include "ZSPI_LED.h"
 #include "ZI2C.h"
 #include "ZSingleWireSerial.h"
 
@@ -64,6 +65,7 @@ struct F4_Settings {
 #define CODAL_PIN ZPin
 #define CODAL_TIMER Timer
 #define CODAL_SPI ZSPI
+#define CODAL_SPI_LED ZSPI_LED
 #define CODAL_I2C ZI2C
 #define CODAL_JACDAC_WIRE_SERIAL codal::ZSingleWireSerial
 
