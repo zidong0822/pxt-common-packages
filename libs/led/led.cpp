c9e8779bcdf80958c32501b4e1ebfa1217dbd635
zi.wang <zi.wang@hypereal.com>
2020-03-03 19:39:03 +0800

    add clear when platform init

--- libs/led/led.cpp
+++ libs/led/led.cpp
@@ -3,19 +3,10 @@
 #include "FSMCIO.h"
 #endif
 
-namespace led {
-
-SPI *spi_led = NULL;
-uint8_t txBuffer[8 * 3 * 4 + 1] = {};
-const uint8_t buf_bytes[4] = {0x88, 0x8E, 0xE8, 0xEE};
-const uint32_t txSize = 8 * 3 * 4 + 1;
-const uint8_t mask = 0x03;
-
-//% blockId=show_color block="show color id %id red %red green %green blue %blue"
-//% group="LED"
-//% inlineInputMode=inline
-void showLED(int id, int red, int green, int blue) {
-    if (!spi_led) {
+namespace pxt {
+class Led {
+public:
+    Led() {
         CODAL_PIN spi_led_pin(DEVICE_ID_IO_P0 + 36, PB_5, PIN_CAPABILITY_DIGITAL);
         CODAL_PIN *NULL_CODAL_PIN = NULL;
         spi_led = new CODAL_SPI(spi_led_pin, *NULL_CODAL_PIN, *NULL_CODAL_PIN);
@@ -26,27 +17,58 @@ void showLED(int id, int red, int green, int blue) {
         memset(txBuffer, 0x88, txSize - 1);
     }
 
-    if (id >= 8) {
-        return;
+    void show(int id, int red, int green, int blue) {
+        if (id >= 8) {
+            return;
+        }
+
+        int index = id * 12;
+        txBuffer[index] = buf_bytes[green >> 6 & mask];
+        txBuffer[index + 1] = buf_bytes[green >> 4 & mask];
+        txBuffer[index + 2] = buf_bytes[green >> 2 & mask];
+        txBuffer[index + 3] = buf_bytes[green & mask];
+
+        txBuffer[index + 4] = buf_bytes[red >> 6 & mask];
+        txBuffer[index + 5] = buf_bytes[red >> 4 & mask];
+        txBuffer[index + 6] = buf_bytes[red >> 2 & mask];
+        txBuffer[index + 7] = buf_bytes[red & mask];
+
+        txBuffer[index + 8] = buf_bytes[blue >> 6 & mask];
+        txBuffer[index + 9] = buf_bytes[blue >> 4 & mask];
+        txBuffer[index + 10] = buf_bytes[blue >> 2 & mask];
+        txBuffer[index + 11] = buf_bytes[blue & mask];
+
+        spi_led->transfer(txBuffer, txSize, NULL, 0);
     }
 
-    int index = id * 12;
-    txBuffer[index] = buf_bytes[green >> 6 & mask];
-    txBuffer[index + 1] = buf_bytes[green >> 4 & mask];
-    txBuffer[index + 2] = buf_bytes[green >> 2 & mask];
-    txBuffer[index + 3] = buf_bytes[green & mask];
+    void clear() {
+        memset(txBuffer, 0x88, txSize - 1);
+        spi_led->transfer(txBuffer, txSize, NULL, 0);
+    }
 
-    txBuffer[index + 4] = buf_bytes[red >> 6 & mask];
-    txBuffer[index + 5] = buf_bytes[red >> 4 & mask];
-    txBuffer[index + 6] = buf_bytes[red >> 2 & mask];
-    txBuffer[index + 7] = buf_bytes[red & mask];
+private:
+    SPI *spi_led = NULL;
+    uint8_t txBuffer[8 * 3 * 4 + 1] = {};
+    const uint8_t buf_bytes[4] = {0x88, 0x8E, 0xE8, 0xEE};
+    const uint32_t txSize = 8 * 3 * 4 + 1;
+    const uint8_t mask = 0x03;
+};
+SINGLETON(Led);
+}
 
-    txBuffer[index + 8] = buf_bytes[blue >> 6 & mask];
-    txBuffer[index + 9] = buf_bytes[blue >> 4 & mask];
-    txBuffer[index + 10] = buf_bytes[blue >> 2 & mask];
-    txBuffer[index + 11] = buf_bytes[blue & mask];
+namespace led {
 
-    spi_led->transfer(txBuffer, txSize, NULL, 0);
+//% blockId=show_color block="show color id %id red %red green %green blue %blue"
+//% group="LED"
+//% inlineInputMode=inline
+void showLED(int id, int red, int green, int blue) {
+    auto spi_led = pxt::getLed();
+    spi_led->show(id, red, green, blue);
+}
+
+void clear() {
+    auto spi_led = pxt::getLed();
+    spi_led->clear();
 }
 
 } // namespace led
---
+++ libs/led/led.h
@@ -0,0 +1,14 @@
+#ifndef __PXT_LED_H
+#define __PXT_LED_H
+
+#include "pxt.h"
+
+namespace led {
+/**
+* Clear spi led
+*/
+void clear();
+
+}
+
+#endif //__PXT_LED_H
